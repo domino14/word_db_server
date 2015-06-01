3653be5a5475f6e29e1d83b53779ca99f96b6683
Cesar Del Solar <delsolar@gmail.com>
2015-06-01 21:29:06 +0300

    added tests for combo counting

---
+++ combinations_test.go
@@ -0,0 +1,39 @@
+package main
+
+import "testing"
+
+type testpair struct {
+	alphagram    string
+	combinations uint64
+}
+
+// OWL2 tests
+var combinationsTests = []testpair{
+	{"AADEEEILMNORSTU", 640342278144},
+	{"AAJQQ", 153},
+	{"ACEIORT", 2323512},
+	{"MMSUUUU", 120},
+	{"AIJNORT", 817236},
+	{"AEFFGINR", 1077300},
+	{"ADEINOPRTTVZ", 860575104},
+	{"ABEIPRSTZ", 5669136},
+}
+
+func TestCalcCombinations(t *testing.T) {
+	lexInfo := LexiconInfo{
+		lexiconName:        "OWL2",
+		gaddagFilename:     "./blah.gaddag",
+		lexiconIndex:       4,
+		descriptiveName:    "American 06",
+		letterDistribution: EnglishLetterDistribution()}
+	lexInfo.Initialize()
+
+	for _, pair := range combinationsTests {
+		combinations := lexInfo.Combinations(pair.alphagram)
+		if combinations != pair.combinations {
+			t.Error("For", pair.alphagram, "expected", pair.combinations,
+				"got", combinations)
+		}
+
+	}
+}
--- dbmaker.go
+++ dbmaker.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	//"database/sql"
-	//_ "github.com/mattn/go-sqlite3"
-	"fmt"
+//"database/sql"
+//_ "github.com/mattn/go-sqlite3"
+//"fmt"
 )
 
 func main() {
@@ -15,13 +15,4 @@ func main() {
 		descriptiveName:    "American 06",
 		letterDistribution: EnglishLetterDistribution()}
 	lexInfo.Initialize()
-	fmt.Println(lexInfo.subChooseCombos)
-	fmt.Println(lexInfo.Combinations("AADEEEILMNORSTU"))
-	fmt.Println(lexInfo.Combinations("AAJQQ"))
-	fmt.Println(lexInfo.Combinations("ACEIORT"))
-	fmt.Println(lexInfo.Combinations("MMSUUUU"))
-	fmt.Println(lexInfo.Combinations("AIJNORT"))
-	fmt.Println(lexInfo.Combinations("AEFFGINR"))
-	fmt.Println(lexInfo.Combinations("ADEINOPRTTVZ"))
-	fmt.Println(lexInfo.Combinations("ABEIPRSTZ"))
 }
