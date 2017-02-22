7d4c60c3efdcf42f13f77f8bc889ee081e99cbae
Cesar Del Solar <delsolar@gmail.com>
2017-02-22 09:09:00 -0800

    Adding America2016 logic, env vars

--- main.go
+++ main.go
@@ -2,6 +2,8 @@
 package main
 
 import (
+	"os"
+
 	"github.com/domino14/macondo/gaddag"
 	"github.com/domino14/macondo/lexicon"
 	"github.com/domino14/word_db_maker/dbmaker"
@@ -10,49 +12,64 @@ import (
 func main() {
 	//db, err := sql.Open("sqlite3", "./"+lexname+".db")
 	symbols := []dbmaker.LexiconSymbolDefinition{
+		{In: "America2016", NotIn: "CSW15", Symbol: "$"},
+		{In: "America2016", NotIn: "America", Symbol: "%"},
+		{In: "America2016", NotIn: "OWL2", Symbol: "+"},
 		{In: "America", NotIn: "CSW15", Symbol: "$"},
 		{In: "America", NotIn: "OWL2", Symbol: "+"},
+		{In: "CSW15", NotIn: "America2016", Symbol: "#"},
 		{In: "CSW15", NotIn: "America", Symbol: "#"},
 		{In: "CSW15", NotIn: "CSW12", Symbol: "+"},
 	}
+	// set LEXICON_PATH to something.
+	// For example "/Users/cesar/coding/webolith/words/" on my computer.
+	lexiconPrefix := os.Getenv("LEXICON_PATH")
 	lexiconMap := dbmaker.LexiconMap{
 		// Pregenerate these gaddags with macondo/gaddag package.
 		"OWL2": lexicon.LexiconInfo{
 			LexiconName:        "OWL2",
-			LexiconFilename:    "/Users/cesar/coding/webolith/words/OWL2.txt",
-			Gaddag:             gaddag.LoadGaddag("/Users/cesar/coding/webolith/words/OWL2.gaddag"),
+			LexiconFilename:    lexiconPrefix + "OWL2.txt",
+			Gaddag:             gaddag.LoadGaddag(lexiconPrefix + "OWL2.gaddag"),
 			LexiconIndex:       4,
 			DescriptiveName:    "American 06",
 			LetterDistribution: lexicon.EnglishLetterDistribution(),
 		},
 		"CSW12": lexicon.LexiconInfo{
 			LexiconName:        "CSW12",
-			LexiconFilename:    "/Users/cesar/coding/webolith/words/CSW12.txt",
-			Gaddag:             gaddag.LoadGaddag("/Users/cesar/coding/webolith/words/CSW12.gaddag"),
+			LexiconFilename:    lexiconPrefix + "CSW12.txt",
+			Gaddag:             gaddag.LoadGaddag(lexiconPrefix + "CSW12.gaddag"),
 			LexiconIndex:       6,
 			DescriptiveName:    "Collins 12",
 			LetterDistribution: lexicon.EnglishLetterDistribution(),
 		},
 		"CSW15": lexicon.LexiconInfo{
-			LexiconName:        "CSW12",
-			LexiconFilename:    "/Users/cesar/coding/webolith/words/CSW15.txt",
-			Gaddag:             gaddag.LoadGaddag("/Users/cesar/coding/webolith/words/CSW15.gaddag"),
+			LexiconName:        "CSW15",
+			LexiconFilename:    lexiconPrefix + "CSW15.txt",
+			Gaddag:             gaddag.LoadGaddag(lexiconPrefix + "CSW15.gaddag"),
 			LexiconIndex:       1,
 			DescriptiveName:    "Collins 15",
 			LetterDistribution: lexicon.EnglishLetterDistribution(),
 		},
 		"America": lexicon.LexiconInfo{
 			LexiconName:        "America",
-			LexiconFilename:    "/Users/cesar/coding/webolith/words/America.txt",
-			Gaddag:             gaddag.LoadGaddag("/Users/cesar/coding/webolith/words/America.gaddag"),
+			LexiconFilename:    lexiconPrefix + "America.txt",
+			Gaddag:             gaddag.LoadGaddag(lexiconPrefix + "America.gaddag"),
 			LexiconIndex:       7,
 			DescriptiveName:    "I am America, and so can you.",
 			LetterDistribution: lexicon.EnglishLetterDistribution(),
 		},
+		"America2016": lexicon.LexiconInfo{
+			LexiconName:        "America2016",
+			LexiconFilename:    lexiconPrefix + "America2016.txt",
+			Gaddag:             gaddag.LoadGaddag(lexiconPrefix + "America2016.gaddag"),
+			LexiconIndex:       2,
+			DescriptiveName:    "I am Trumperica, and so can you.",
+			LetterDistribution: lexicon.EnglishLetterDistribution(),
+		},
 		"FISE": lexicon.LexiconInfo{
 			LexiconName:        "FISE09",
-			LexiconFilename:    "/Users/cesar/coding/webolith/words/FISE.txt",
-			Gaddag:             gaddag.LoadGaddag("/Users/cesar/coding/webolith/words/FISE.gaddag"),
+			LexiconFilename:    lexiconPrefix + "FISE.txt",
+			Gaddag:             gaddag.LoadGaddag(lexiconPrefix + "FISE.gaddag"),
 			LexiconIndex:       8,
 			DescriptiveName:    "Federación Internacional de Scrabble en Español",
 			LetterDistribution: lexicon.SpanishLetterDistribution(),
