04064eec0b7fdbb2be3e158e416303983601e47e
Cesar Del Solar <delsolar@gmail.com>
2019-06-09 20:45:25 -0400

    add one more test

--- internal/searchserver/expand_test.go
+++ internal/searchserver/expand_test.go
@@ -35,3 +35,26 @@ func TestExpand(t *testing.T) {
 		resp.Alphagrams[1].Words[0].Definition,
 	)
 }
+
+// Test that the chunking will work ok.
+func TestExpandHuge(t *testing.T) {
+	// query a few thousand words with expand false
+	req := WordSearch([]*pb.SearchRequest_SearchParam{
+		SearchDescLexicon("NWL18"),
+		SearchDescLength(8, 8),
+		SearchDescProbRange(3060, 6060),
+	}, false)
+	resp, _ := searchHelper(req)
+
+	s := &Server{
+		LexiconPath: os.Getenv("LEXICON_PATH"),
+	}
+	expandedResp, err := s.Expand(context.Background(), resp)
+	assert.Nil(t, err)
+	assert.Equal(t, 3001, len(expandedResp.Alphagrams))
+	assert.True(t, len(expandedResp.Alphagrams[3000].Words) > 0)
+	assert.True(t, len(expandedResp.Alphagrams[3000].Words[0].Definition) > 0)
+	assert.Equal(t, expandedResp.Alphagrams[3000].Words[0].Alphagram,
+		expandedResp.Alphagrams[3000].Alphagram)
+
+}
