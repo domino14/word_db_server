f89aa58684790d56857156f37e8105ba49a63ee1
César Del Solar <delsolar@gmail.com>
2024-09-25 08:43:52 -0400

    fix 500 again

--- internal/wordvault/server.go
+++ internal/wordvault/server.go
@@ -107,10 +107,10 @@ func (s *Server) GetNextScheduled(ctx context.Context, req *connect.Request[pb.G
 		NextScheduled: pgtype.Timestamptz{Time: s.Nower.Now(), Valid: true},
 	})
 	if err != nil {
-		if errors.Is(err, pgx.ErrNoRows) {
-			return connect.NewResponse(&pb.Cards{}), nil
-		}
 		return nil, err
+	}
+	if len(rows) == 0 {
+		return connect.NewResponse(&pb.Cards{}), nil
 	}
 	cards := make([]*pb.Card, len(rows))
 
