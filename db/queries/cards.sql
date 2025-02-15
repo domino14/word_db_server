d2d4f4dbb795aa98dfba8d0b8ce400361a42bd91
Ben Muschol <benmuschol@gmail.com>
2025-02-14 19:14:52 -0500

    Add query

--- db/queries/cards.sql
+++ db/queries/cards.sql
@@ -131,6 +131,11 @@ WITH inserted_rows AS (
 )
 SELECT COUNT(*) FROM inserted_rows;
 
+-- name: MoveCards :execrows
+UPDATE wordvault_cards
+SET deck_id = $3
+WHERE user_id = $1 AND lexicon_name = $2 AND alphagram = ANY(@alphagrams::text[]);
+
 -- name: GetNextScheduledBreakdown :many
 WITH scheduled_cards AS (
     SELECT
