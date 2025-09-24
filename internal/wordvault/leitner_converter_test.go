103b06d12803bd995c9050da6b92500995623655
César Del Solar <delsolar@gmail.com>
2025-09-23 22:51:56 -0400

    try to fix xerafin import bug

--- internal/wordvault/leitner_converter_test.go
+++ internal/wordvault/leitner_converter_test.go
@@ -15,8 +15,8 @@ func TestConvertToFSRS(t *testing.T) {
 	is := is.New(t)
 	// Convert a card we've never missed.
 	now := time.Date(2024, 11, 14, 3, 4, 5, 0, time.UTC)
-	card, revLog, _ := convertLeitnerToFsrs(21, 0, 21, sql.NullInt32{Int32: 1712380948, Valid: true},
-		sql.NullInt32{Int32: 1721117219, Valid: true},
+	card, revLog, _ := convertLeitnerToFsrs(21, 0, 21, int64(1712380948),
+		int64(1721117219),
 		sql.NullInt32{Int32: 7, Valid: true},
 		now)
 
@@ -38,8 +38,8 @@ func TestConvertToFSRS(t *testing.T) {
 	// Convert a card that was last correct after it was due. This can happen
 	// if you quiz on it outside of cardbox.
 	card, revLog, _ = convertLeitnerToFsrs(2, 2, 1,
-		sql.NullInt32{Int32: 1730935552, Valid: true},
-		sql.NullInt32{Int32: 1730330752, Valid: true},
+		int64(1730935552),
+		int64(1730330752),
 		sql.NullInt32{Int32: 1, Valid: true},
 		now)
 	// stability should be handwaved to a small number
@@ -49,4 +49,12 @@ func TestConvertToFSRS(t *testing.T) {
 	newCard = schedulingCards[rating].Card
 	is.True(!math.IsNaN(newCard.Stability))
 	fmt.Println("newCard", newCard.Stability, newCard.Due, newCard.Difficulty)
+
+	// Test handling of scientific notation string (the bug we're fixing)
+	card, _, _ = convertLeitnerToFsrs(5, 1, 4,
+		"1712380948.0",
+		"1.756833312857143e+09", // This is the problematic value from the issue
+		sql.NullInt32{Int32: 3, Valid: true},
+		now)
+	is.True(card.Stability > 0) // Should parse correctly and not crash
 }
